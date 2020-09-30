cb70aa096dfe12f705c0029816108984250ccc7c
Nimmy Sambasivan <nimmy.sambasivan@sapient.com>
2020-09-30 16:19:53 +0530

    Reset filter fix

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            applyFilter: false
+            applyFilter: window.location.search? true: false
         };
     }
     applyFilter(isApplied) {
